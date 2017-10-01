fe864997693a9ba48903939024f011abf3ea4cd8
Raphael <raphael@mresolucoes.com.br>
2017-09-30 23:16:57 -0300

    Configure initial unit test enviroment
    - Add new `core-js` dependencies of React 16 (now importing `map` and `set` in `index.js`) and `raf` dependency for running tests without annoying warnings about the need for a polyfill for older browsers (one warning for each test suite or so, seriously?);
    - Add a new parameter to `test` script to get ride of `raf` warnings;
    - Remove App.test.js at least for now (App.js renders a component that uses Redux Connect(Component) and that cause issues to run the tests);
    - Add `tests` folders to `actions`, `components` and `reducers` folders;
    - Add some simples unit tests to get started with testing stuff;

---
+++ src/components/tests/Input.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Input from '../Input';
+
+describe('Input', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Input />, div);
+  })
+  // TODO: Start using enzyme and add more tests to the component.
+});
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux'
 import store from './Store'
+import { Provider } from 'react-redux'
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <App />
     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
