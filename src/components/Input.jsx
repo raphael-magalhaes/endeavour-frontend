16e8cba4e02c66d62117470b11bff96fad9cfde4
Raphael <raphael.sdm@gmail.com>
2017-10-18 20:49:21 -0200

    Add ESLint and Pretty dependencies
    Add .eslintrc;
    Add husky and lint-staged;
    Add lint-staged command to lint staged files before commit;
    Testing if prettier is working.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from 'material-ui/styles'
-import Button from 'material-ui/Button'
-import Typography from 'material-ui/Typography'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
 
 const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit
-    }
-})
+  button: {
+    margin: theme.spacing.unit
+  }
+});
 
+// Testing Prettier linting
 const Input = props => {
-    const { value, onClick, classes } = props
-    return (
-        <div>
-            <Typography gutterBottom noWrap>
-                {value}
-            </Typography>
-            <Button onClick={() => { onClick("Redux Works: " + new Date()) }} color="primary" className={classes.button}>
-                Submit
-            </Button>
-        </div>
-    )
-}
+  const { value, onClick, classes } = props;
+  return (
+    <div>
+      <Typography gutterBottom noWrap>
+        {value}
+      </Typography>
+      <Button
+        onClick={() => {
+          onClick('Redux Works: ' + new Date());
+        }}
+        color="primary"
+        className={classes.button}
+      >
+        Submit
+      </Button>
+    </div>
+  );
+};
 
 Input.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired
+};
 
-export default withStyles(styles)(Input)
+export default withStyles(styles)(Input);
